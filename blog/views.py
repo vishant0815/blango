5f58bc342e3ed5fb578510a0c5885f710f430381
Vishant Bhavsar <vishantbhavsar500@gmail.com>
2025-10-01 10:50:47 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,13 @@ from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
 
+
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
   
 def post_detail(request, slug):
