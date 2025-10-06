0db9dd7981e0b740517fe0577c20f32320727db2
Vishant Bhavsar <vishantbhavsar500@gmail.com>
2025-10-06 07:18:16 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -40,6 +40,9 @@ urlpatterns += [
         name="schema-swagger-ui",
     ),
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
-
-
